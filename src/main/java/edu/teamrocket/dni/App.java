6ed9ec2ebaf9f893c4fdd31693a7467a7b2d01b1
Sara <saragarciagarrido96@gmail.com>
2026-03-23 13:19:22 +0100

    feat(App.java): Crear la App.java

--- src/main/java/edu/teamrocket/dni/App.java
+++ src/main/java/edu/teamrocket/dni/App.java
@@ -2,6 +2,19 @@ package edu.teamrocket.dni;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        TablaAsignacion tablaAsignacion = new TablaAsignacion();
+        Dni dni = new Dni("663645805S");
+
+        boolean letraPermitida = tablaAsignacion.esLetraPermitida(dni.getParteAlfabetica());
+        boolean valido = dni.getNumeroSano() && letraPermitida && dni.checkDniValido();
+
+        if (valido) {
+            System.out.println("DNI valido: " + dni.getDniOriginal());
+        } else {
+            System.out.println("DNI no valido: " + dni.getDniOriginal());
+            System.out.println("Letra esperada para " + dni.getParteNumerica() + ": " + dni.getLetraCalculada());
+        }
+
     }
 }
